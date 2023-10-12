78b39a53e906116b52df5d5bfd2489fb3ce331cb
lhelmi <7m.helmi@gmail.com>
2023-10-12 14:43:28 +0700

    unittest fix

--- test/user.test.ts
+++ test/user.test.ts
@@ -3,11 +3,12 @@ import { web } from "../src/app/Web";
 import { logger } from "../src/app/Logger";
 import { removeTestUser } from "./TestUtil";
 import mongoose from "mongoose";
+import config from "../src/config/Config";
 
 describe('POST /api/users/register', function() {
 
     beforeEach(async() => {
-        
+        await mongoose.connect(`mongodb://${config.dbUser}:${config.dbPass}@${config.dbHost}:${config.dbPort}/${config.dbName}?authSource=admin`);
     });
 
     afterEach(async() => {
@@ -34,7 +35,27 @@ describe('POST /api/users/register', function() {
         expect(result.body.data.customer_id).toBe(20);
     });
 
-    it('should rejected : registration ', async() => {
+    it('should rejected : full_name, password is empty ', async() => {
+        
+        const result = await supertest(web)
+        .post('/api/users/register')
+        .send({
+            full_name : '',
+            customer_id : 20,
+            email : 'testtes.cc', //the error
+            password : ''
+        });
+        
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined;
+        expect(result.body.field).toBeDefined;
+        expect(result.body.field.email).toBeDefined;
+        
+    });
+
+    it('should rejected : duplicate email account ', async() => {
         
         const result = await supertest(web)
         .post('/api/users/register')
@@ -43,6 +64,26 @@ describe('POST /api/users/register', function() {
             customer_id : 20,
             // email : 'testtes.cc', //the error
             email : 'gold@go.ld', //the error : duplicate email account
+            password : 'test'
+        });
+        
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined;
+        expect(result.body.field).toBeDefined;
+        expect(result.body.field.email).toBeDefined;
+        
+    });
+
+    it('should rejected : invalid email ', async() => {
+        
+        const result = await supertest(web)
+        .post('/api/users/register')
+        .send({
+            full_name : 'test',
+            customer_id : 20,
+            email : 'testtes.cc', //the error
             password : 'test'
         });
         
